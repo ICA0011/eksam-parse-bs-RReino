ee9e9d27ab0124cd4de2ba3c42abcc0b58b4e3d5
Romet Reino <rreino@ttu.ee>
2022-05-26 12:28:27 +0300

    Fixed parse_xml.py

--- parse_xml.py
+++ parse_xml.py
@@ -2,11 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
+
 def parse_xml():
-  url = "http://upload.itcollege.ee/~aleksei/test.xml"
-  xml_content = requests.get(url).content
-  soup = BeautifulSoup(xml_content,'xml')
-  
-  # your code here
-  
-  return result
+    url = "http://upload.itcollege.ee/~aleksei/test.xml"
+    xml_content = requests.get(url).content
+    soup = BeautifulSoup(xml_content, 'xml')
+
+    find_data = soup.find_all('data')
+    for data in find_data:
+        text = data.get_text().strip()
+        return text
+
+print(parse_xml())
